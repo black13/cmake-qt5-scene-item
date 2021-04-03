e63d95381942b6ca2618727a726456f07b68aa3f
black13 <jjosburn@gmail.com>
2021-04-03 14:31:52 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QDesktopWidget>
+#include <QList>
+#include <QMap>
+#include <QTimer>
+;
+
+int main(int argc, char** argv)
+{
+
+	QApplication app(argc, argv);
+
+	
+	return app.exec();
+}
