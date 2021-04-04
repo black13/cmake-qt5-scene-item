5a8133d33df3b7a0e2bde9ca47798f11c96e5dd2
black13 <jjosburn@gmail.com>
2021-04-04 10:47:50 -0600

    add simple item

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <QMainWindow>
 #include <QDesktopWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QList>
 #include <QMap>
 #include <QTimer>
@@ -12,6 +14,11 @@ int main(int argc, char** argv)
 
 	QApplication app(argc, argv);
 
+	QGraphicsScene scene;
+	scene.addText("Hello, world!");
+
+	QGraphicsView view(&scene);
+	view.show();
 	
 	return app.exec();
 }
